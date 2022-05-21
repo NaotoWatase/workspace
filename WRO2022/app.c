4495acecc83980ccc13f3e37ecb26acc81389d3c
Na0toWatabe <82302069+NaotoWatase@users.noreply.github.com>
2022-05-21 10:27:12 +0900

    main

--- WRO2022/app.c
+++ WRO2022/app.c
@@ -104,8 +104,7 @@ void tank_turn(float angle, int power_L, int power_R){
 void tank_turn_color(int power_L, int power_R){
     
     colorid_t color_check = COLOR_NONE;
-    (void)ev3_motor_rotate(EV3_PORT_B, 10000000, (int16_t)-power_L, false);
-    (void)ev3_motor_rotate(EV3_PORT_C, 10000000, (int16_t)power_R, false);
+
     while(color_check != COLOR_WHITE) {
         if(power_L > 0) {
             color_check = ev3_color_sensor_get_color(EV3_PORT_2);
@@ -260,5 +259,5 @@ void main_task(intptr_t unused) {
             break;
     }
     while(1) {}*/
-    tank_turn_color(5, -5);
+    steering_color(COLOR_WHITE, 30, 50);
 }   
